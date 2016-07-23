333697c12c85be85938515b1b5ab901c4917065b
Matt Pindell <pindell.m@gmail.com>
2016-07-23 13:46:40 -0600

    Refactor to use arrow function

--- binary_search.js
+++ binary_search.js
@@ -1,32 +1,25 @@
 'use strict'
 
-class BinarySearch {
-  constructor(array) {
-    this.array = array
-  }
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-  binarySearch(item) {
-    let low = 0
-    let high = this.array.length - 1
+let binarySearch = (array, item) => {
+  let low = 0
+  let high = array.length - 1
 
-    while (low <= high) {
-      let mid = Math.floor((low + high) / 2)
-      let guess = this.array[mid]
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2)
+    let guess = array[mid]
 
-      if (guess === item) { return mid }
-      if (guess > item) {
-        high = mid - 1
-      } else {
-        low = mid + 1
-      }
+    if (guess === item) { return mid }
+    if (guess > item) {
+      high = mid - 1
+    } else {
+      low = mid + 1
     }
-    return null
   }
+  return null
 }
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const bs = new BinarySearch(array)
-
-console.log(bs.binarySearch(1));
-console.log(bs.binarySearch(10));
-console.log(bs.binarySearch(-1));
+console.log(binarySearch(array, 3));
+console.log(binarySearch(array, 10));
+console.log(binarySearch(array, -1));
