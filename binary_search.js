b5353d4af7005f1ed35675d42022e60a31f84312
Matt Pindell <pindell.m@gmail.com>
2016-07-23 13:28:08 -0600

    Add binary_search

--- binary_search.js
+++ binary_search.js
@@ -1,24 +1,32 @@
+'use strict'
+
 class BinarySearch {
   constructor(array) {
     this.array = array
   }
 
-  search(item) {
-    if (this.array.includes(item)) {
-      low = 0
-      high = this.array.length
+  binarySearch(item) {
+    let low = 0
+    let high = this.array.length - 1
 
-      while low <= high {
-        mid = low + high
-        guess = this.array[mid]
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2)
+      let guess = this.array[mid]
 
-        if guess === item { return mid }
-        if guess > item {
-          high = mid--
-        } else {
-          low = mid++
-        }
+      if (guess === item) { return mid }
+      if (guess > item) {
+        high = mid - 1
+      } else {
+        low = mid + 1
       }
     }
+    return null
   }
 }
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const bs = new BinarySearch(array)
+
+console.log(bs.binarySearch(1));
+console.log(bs.binarySearch(10));
+console.log(bs.binarySearch(-1));
