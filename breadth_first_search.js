91845f4012db232aa2e170db6318567a890fe520
Matt Pindell <pindell.m@gmail.com>
2016-08-22 21:48:15 -0400

    Refactor search function

--- breadth_first_search.js
+++ breadth_first_search.js
@@ -9,16 +9,15 @@ class Graph {
   add(key, value) {
     this.content.set(key, value)
   }
-
+  
   search(name) {
     let search_queue = this.content.get(name),
         searched = []
-
     while (search_queue.length !== 0) {
       let person = search_queue.shift()
       if (!_.includes(searched, person)) {
-        if (_.endsWith(person,'m')) {
-          console.log(person + " is a mango seller!")
+        if (person_is_seller(person)) {
+          console.log(person + " is a seller!")
           return true
         } else {
           let new_connections = this.content.get(person)
@@ -29,6 +28,10 @@ class Graph {
     }
     return false
   }
+}
+
+let person_is_seller = (name) => {
+  if (_.endsWith(name, 'm')) { return true }
 }
 
 let graph = new Graph
