9ecfd0a895642fc7bdaa721babb729d558956ff1
Matt Pindell <pindell.m@gmail.com>
2016-08-30 19:05:59 -0400

    Add recursive fibonacci example

--- basic_recursion.js
+++ basic_recursion.js
@@ -11,6 +11,12 @@ let length = (array) => {
   return 1 + length(array)
 }
 
+let fibonacci = (n) => {
+  if (n <= 1) { return n }
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
 let arr = [1, 2, 3, 4, 5]
-console.log("The sum is:", sum(arr));
-console.log("The length is:", length(arr));
+console.log("The sum is:", sum(arr))
+console.log("The length is:", length(arr))
+console.log("Given 10, fibonacci returns:", fibonacci(10) )
