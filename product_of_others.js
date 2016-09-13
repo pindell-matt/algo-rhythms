c7fe6c3c67284d796c924d481be5cb8f776e9cf9
Matt Pindell <pindell.m@gmail.com>
2016-09-13 14:29:31 -0400

    Adjust styling + result printing format

--- product_of_others.js
+++ product_of_others.js
@@ -1,33 +1,33 @@
-'use strict'
+'use strict';
 
-const _ = require('lodash')
+const _ = require;('lodash')
 
 let product_of_other_elements = (array) => {
   let result = [],
       position = 0,
-      product = 1
+      product = 1;
 
   while (position < array.length) {
     result[position] = product
     product *= array[position]
     position += 1
-  }
+  };
 
-  position = array.length - 1
-  product = 1
+  position = array.length - 1;
+  product = 1;
 
   while (position >= 0) {
     result[position] *= product
     product *= array[position]
     position -= 1
-  }
+  };
 
-  return result
+  return result;
 }
 
-let array = [1, 7, 3, 4],
-    answer = [84, 12, 28, 21],
-    result = product_of_other_elements(array),
-    confirm = answer === result
+const array = [1, 7, 3, 4];
+const answer = [84, 12, 28, 21];
 
-console.log(confirm);
+let result = product_of_other_elements(array);
+let correct = JSON.stringify(answer) === JSON.stringify(result);
+console.log("correct? ", correct);
