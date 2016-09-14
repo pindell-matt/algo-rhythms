18159f77012fe831841d96e77e06a5c036fe4771
Matt Pindell <pindell.m@gmail.com>
2016-09-14 14:48:46 -0400

    Stylistic refactoring

--- product_of_others.js
+++ product_of_others.js
@@ -1,25 +1,23 @@
 'use strict';
 
-const _ = require;('lodash')
-
-let product_of_other_elements = (array) => {
+function productOfOthers(array) {
   let result = [],
       position = 0,
       product = 1;
 
   while (position < array.length) {
-    result[position] = product
-    product *= array[position]
-    position += 1
+    result[position] = product;
+    product *= array[position];
+    position += 1;
   };
 
   position = array.length - 1;
   product = 1;
 
   while (position >= 0) {
-    result[position] *= product
-    product *= array[position]
-    position -= 1
+    result[position] *= product;
+    product *= array[position];
+    position -= 1;
   };
 
   return result;
@@ -28,6 +26,8 @@ let product_of_other_elements = (array) => {
 const array = [1, 7, 3, 4];
 const answer = [84, 12, 28, 21];
 
-let result = product_of_other_elements(array);
+let result = productOfOthers(array);
 let correct = JSON.stringify(answer) === JSON.stringify(result);
+
 console.log("correct? ", correct);
+console.log(result);
