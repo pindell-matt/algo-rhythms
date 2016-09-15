4e649d0fbbdb6d5aaaf3afa0cb24fcaf321b5d1d
Matt Pindell <pindell.m@gmail.com>
2016-09-15 17:20:11 -0400

    Reformat for style

--- binary_search.js
+++ binary_search.js
@@ -1,37 +1,38 @@
-'use strict'
-const _ = require('lodash')
+'use strict';
 
-const array = _.range(150)
-
-let binarySearch = (array, item) => {
+function binarySearch(array, item) {
   let low = 0,
-      high = array.length - 1
+      high = array.length - 1;
   while (low <= high) {
     let mid = Math.floor((low + high) / 2),
-        guess = array[mid]
-    if (guess === item) { return `found ${mid}` }
+        guess = array[mid];
+    if (guess === item) return `found ${mid}`;
     if (guess > item) {
-      high = mid - 1
+      high = mid - 1;
     } else {
-      low = mid + 1
-    }
+      low = mid + 1;
+    };
   }
-  return null
+  return `No match for ${item}`;
 }
 
-let recursiveBinarySearch = (array, item) => {
-  let mid = Math.ceil((array.length - 1) / 2)
+function recursiveBinarySearch(array, item) {
+  let mid = Math.ceil((array.length - 1) / 2);
   while (array.length > 2) {
-    if (array[mid] === item) { return `found ${array[mid]}` }
+    if (array[mid] === item) return `found ${array[mid]}`;
 
-    let upperRange = array.slice(mid, array.length)
-    if (array[mid] < item) { return recursiveBinarySearch(upperRange, item) }
+    let upperRange = array.slice(mid, array.length);
+    if (array[mid] < item) return recursiveBinarySearch(upperRange, item);
 
-    let lowerRange = array.slice(0, mid)
-    if (array[mid] > item) { return recursiveBinarySearch(lowerRange, item) }
+    let lowerRange = array.slice(0, mid);
+    if (array[mid] > item) return recursiveBinarySearch(lowerRange, item);
   }
-  if (array[mid] === item) { return `found ${array[mid]}` } else { return null }
+  if (array[mid] === item) return `found ${array[mid]}`;
+  return `No match for ${item}`;
 }
+
+const _ = require('lodash');
+const array = _.range(150)
 
 console.log(binarySearch(array, 3));
 console.log(binarySearch(array, 100));
