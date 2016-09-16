2b7ce50dcc4599ace1c8c1a836684ae67803ecfe
Matt Pindell <pindell.m@gmail.com>
2016-09-16 18:10:36 -0400

    Stylistic refactor

--- breadth_first_search.js
+++ breadth_first_search.js
@@ -1,46 +1,48 @@
-'use strict'
-const _ = require('lodash')
+'use strict';
+
+const _ = require('lodash');
 
 class Graph {
   constructor() {
-    this.content = new Map()
+    this.content = new Map();
   }
 
   add(key, value) {
-    this.content.set(key, value)
+    this.content.set(key, value);
   }
 
   search(name) {
-    let search_queue = this.content.get(name),
-        searched = []
-    while (search_queue.length !== 0) {
-      let person = search_queue.shift()
+    let searchQueue = this.content.get(name),
+        searched = [];
+    while (searchQueue.length !== 0) {
+      let person = searchQueue.shift();
       if (!_.includes(searched, person)) {
-        if (person_is_seller(person)) {
-          console.log(person + " is a seller!")
-          return true
+        if (checkIfSeller(person)) {
+          console.log(`${person} is a seller!`);
+          return true;
         } else {
-          let new_connections = this.content.get(person)
-          search_queue.push(...new_connections)
-          searched.push(person)
+          let newConnections = this.content.get(person);
+          searchQueue.push(...newConnections);
+          searched.push(person);
         }
       }
     }
-    return false
+    return false;
   }
 }
 
-let person_is_seller = (name) => {
-  if (_.endsWith(name, 'm')) { return true }
+function checkIfSeller(name) {
+  // can be changed to whatever indicates 'seller' status
+  if (_.endsWith(name, 'm')) return true;
 }
 
-let graph = new Graph
-graph.add("you", ["alice", "bob", "claire"])
-graph.add("bob", ["anuj", "peggy"])
-graph.add("alice", ["peggy"])
-graph.add("claire", ["tom"])
-graph.add("anuj", [])
-graph.add("peggy", [])
-graph.add("tom", [])
+let graph = new Graph();
+graph.add("you", ["alice", "bob", "claire"]);
+graph.add("bob", ["anuj", "peggy"]);
+graph.add("alice", ["peggy"]);
+graph.add("claire", ["tom"]);
+graph.add("anuj", []);
+graph.add("peggy", []);
+graph.add("tom", []);
 
-graph.search("you")
+graph.search("you");
