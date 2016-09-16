65243bea3efab35f0a6b881c49dcde9341e4df3c
Matt Pindell <pindell.m@gmail.com>
2016-09-16 18:15:34 -0400

    Further stylistic refactoring

--- highest_product_of_3.js
+++ highest_product_of_3.js
@@ -1,21 +1,15 @@
 'use strict';
 
-const arrayOfInts = [1, 10, -5, 1, -100];
-const answer = 5000;
-
 function highestProductOf3(array) {
   if (array.length < 3) throw new Error('Need more than 3 integers!');
 
   let [lowest, highest] = array.slice(0, 2).sort();
-
   let highestProductOf2 = [lowest, highest].reduce((a, x) => a *= x);
   let lowestProductOf2 = [lowest, highest].reduce((a, x) => a *= x);
-
   let highestProductOf3 = array.slice(0, 3).reduce((a, x) => a *= x);
 
   for (var i = 2; i < array.length; i++) {
     var current = array[i];
-
     highestProductOf3 = Math.max(
       highestProductOf3,
       current * highestProductOf2,
@@ -40,6 +34,8 @@ function highestProductOf3(array) {
   return highestProductOf3;
 }
 
+const arrayOfInts = [1, 10, -5, 1, -100];
+const answer = 5000;
 let result = highestProductOf3(arrayOfInts);
-let correct = JSON.stringify(answer) === JSON.stringify(result)
-console.log("correct? ", correct)
+let correct = JSON.stringify(answer) === JSON.stringify(result);
+console.log("correct? ", correct);
