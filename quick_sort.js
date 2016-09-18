1b18a56564c597aa9b20fc8dbacd1f975f8058bc
Matt Pindell <pindell.m@gmail.com>
2016-09-18 10:47:06 -0400

    Refactor quick sort

--- quick_sort.js
+++ quick_sort.js
@@ -1,18 +1,18 @@
-'use strict'
-const _ = require('lodash')
+'use strict';
+
+const _ = require('lodash');
 
 let quickSort = (array) => {
-  if (array.length < 2) {
-    return array
-  } else {
-    let pivot = array[0],
-        lessThan = _.filter(array, (num) => { return num < pivot }),
-        greaterThan = _.filter(array, (num) => { return num > pivot })
-    return _.concat(quickSort(lessThan), pivot, quickSort(greaterThan))
-  }
+  if (array.length < 2) return array;
+  let pivot = array[0],
+      lessThan = _.filter(array, (num) => { return num < pivot }),
+      greaterThan = _.filter(array, (num) => { return num > pivot });
+  return _.concat(quickSort(lessThan), pivot, quickSort(greaterThan));
 }
 
-let array = _.range(50)
-let random = _.shuffle(array)
-let sorted = quickSort(random)
-console.log(_.isEqual(array, sorted))
+let array = _.range(50);
+let random = _.shuffle(array);
+let sorted = quickSort(random);
+let correct = _.isEqual(array, sorted);
+
+console.log(`sorted === array: ${correct}`);
