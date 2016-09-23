a901cefa459fdb6bade5ba7a66ab947df5f69aed
Matt Pindell <pindell.m@gmail.com>
2016-09-23 17:06:22 -0400

    Refactor to use proper es6 style

--- two_sum.js
+++ two_sum.js
@@ -1,34 +1,34 @@
 'use strict';
 
-let initialTwoSum = (nums, target) => {
+function initialTwoSum(nums, target) {
   let base = 0,
       index = 1;
   while (base < nums.length) {
-    var a = nums[base];
-    var b = nums[index];
+    let a = nums[base],
+        b = nums[index];
     if (a + b === target) return [base, index]
     if (index === nums.length - 1) {
-      base++
-      index = base + 1
+      base += 1;
+      index = base + 1;
     } else {
-      index++
+      index += 1;
     }
   }
 };
 
-let refinedTwoSum = (nums, target) => {
+function refinedTwoSum(nums, target) {
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
     let complement = target - nums[i];
-    if (map.has(complement)) return [map.get(complement), i]
-    map.set(nums[i], i)
+    if (map.has(complement)) return [map.get(complement), i];
+    map.set(nums[i], i);
   }
 };
 
 let nums = [2, 1, 9, 4, 4, 56, 90, 3],
     target = 8,
-    expected = [3, 4];
+    answer = [3, 4],
+    result = initialTwoSum(nums, target),
+    correct = JSON.stringify(answer) === JSON.stringify(result);
 
-console.log('expected: ', expected,
-            'initial: ', initialTwoSum(nums, target),
-            'refined: ', refinedTwoSum(nums, target));
+console.log(`result of ${result} is correct: ${correct}`)
